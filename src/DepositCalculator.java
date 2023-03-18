977ba17fe39ecba023b2330363a67b484580525c
Ilya Sheyman <ilyasheiman@yandex.ru>
2023-03-18 19:45:05 +0300

    Edit 18 03

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d ) {
+    double calculateComplexPercent(double a, double y, int d) {
         double pay = a * Math.pow((1 + y / 12), 12 * d);
         return round(pay, 2);
     }
 
-    double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
-        return round(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+    double calculateSimplePercent(double doubleAmount, double yearRate, int depositPeriod) {
+        return round(doubleAmount + doubleAmount * yearRate * depositPeriod, 2);
     }
 
     double round(double value, int places) {
-       double ScaLe = Math.pow(10, places);
-       return Math.round(value * ScaLe) / ScaLe;
+        double ScaLe = Math.pow(10, places);
+        return Math.round(value * ScaLe) / ScaLe;
     }
 
-    void programMenu( ) {
+    void programMenu() {
         int period, action;
         Scanner abcdef = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:");
         int amount = abcdef.nextInt();
         System.out.println("Введите срок вклада в годах:");
-        period = abcdef.nextInt( );
-        System.out.println   ("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        period = abcdef.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = abcdef.nextInt();
 
         double out = 0;
